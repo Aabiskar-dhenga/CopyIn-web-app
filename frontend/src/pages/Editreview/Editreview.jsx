cb06d4e72cb8746e5130a6b50ef69ebdb48153fd
Aabiskar <Copywriterayan47@gmail.com>
2023-07-07 10:15:56 +0545

    bug fixed

--- frontend/src/pages/Editreview/Editreview.jsx
+++ frontend/src/pages/Editreview/Editreview.jsx
@@ -4,18 +4,35 @@ const Review = () => {
   return (
     <div className={styles.editreviewParent}>
       <h1>Editing Review Review name</h1>
-      <div className={styles.uploadDiv}></div>
-      <div className={styles.uploadWrapper}>
-        {/* icon??? */}
-        <div>
-          <input type="text" placeholder="upload avatar" />
-          {/* icon */}
+      <div className={styles.uploadDiv}>
+        <div className={styles.uploadWrapper}>
+          {/* icon??? */}
+          <div>
+            <input type="text" placeholder="upload avatar" />
+            {/* icon */}
+          </div>
+        </div>
+        <div className={styles.dateWrapper}>
+          <input type="date" />
+          {/* icons */}
         </div>
       </div>
-      <div className={styles.dateWrapper}>
-        <input type="date" />
-        {/* icons */}
+      <div className={styles.namebioDiv}>
+        <p>Name </p>
+        <p>Bio</p>
       </div>
+      <div>
+        <input type="text" placeholder="Aabiskar" />
+        <input type="text" placeholder="Dhenga" />
+      </div>
+      <div className="buttonWrapper">
+        <button>Video , Audio , Image</button>
+      </div>
+      <div>
+        <input type="text" placeholder="esssssssssssssjjjjh" />
+        <input type="text" placeholder="Supporte a markdown" />
+      </div>
+      <textarea name=""></textarea>
     </div>
   );
 };
