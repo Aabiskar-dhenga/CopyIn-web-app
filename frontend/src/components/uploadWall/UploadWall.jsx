1df94350da6ef796baddd74d56a3fec544e9d7d9
Rajiv Shrestha <rajivnewar123@gmail.com>
2023-07-07 18:54:10 +0545

    designed uploadWall

--- frontend/src/components/uploadWall/UploadWall.jsx
+++ frontend/src/components/uploadWall/UploadWall.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "./uploadWall.module.css";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { uploadWallColorsArr } from "../../utils/data";
+import { color } from "framer-motion";
+import { RiAncientGateFill } from "react-icons/ri";
 
 const UploadWall = () => {
+  const fileRef = useRef();
+  const [selectedColor, setSelectedColor] = useState("#6346FA");
   return (
     <>
       <div className={styles.uploadWall_container}>
@@ -9,35 +15,72 @@ const UploadWall = () => {
           <h1 className={styles.wall_heading}>
             Editing <span>Mural name</span>
           </h1>
-          <p>
+          <p className={styles.wallHeading_para}>
             After creation, you will have a dedicated wall to murder distrust of
             your potential customers.
           </p>
           <form className={styles.form}>
-            <div className={styles.wallName}>
+            <div className={styles.inputItem}>
               <label>
                 Wall name <span>*</span>
               </label>
-              <input type="text" placeholder="choosenname" />
+              <input type="text" placeholder="choosenname" required />
               <p>our url will be: domain.com/wall/choosenname</p>
             </div>
-            <div className="wall_avatar">
-              <div className={styles.upload_avatar}></div>
-              <div className={styles.customize_avatar}></div>
+            <div className={styles.wall_avatar}>
+              <div className={styles.upload_avatar}>
+                <h2>Wall avatar</h2>
+                <div className={styles.wall_avatar_img_wrapper}>
+                  <RiAncientGateFill />
+                </div>
+
+                <button
+                  className={styles.uploadAvatar_wrapper}
+                  onClick={() => fileRef.current.click()}
+                >
+                  <p>Upload avatar</p>
+                  <AiOutlineCloudUpload />
+                </button>
+                <input ref={fileRef} style={{ display: "none" }} type="file" />
+              </div>
+              <div className={styles.customize_avatar}>
+                <h2>Customize</h2>
+                <div className={styles.choose_colors_container}>
+                  {uploadWallColorsArr.map((colors) => (
+                    <div
+                      onClick={() => setSelectedColor(colors)}
+                      style={{ background: colors }}
+                      className={`${styles.choose_colors} ${
+                        selectedColor === colors && styles.current_color
+                      }`}
+                    ></div>
+                  ))}
+                </div>
+                <div className={styles.customize_field}>
+                  <div
+                    style={{ background: selectedColor }}
+                    className={styles.selected_color}
+                  ></div>
+                  <p className={styles.selected_color_code}>{selectedColor}</p>
+                </div>
+              </div>
             </div>
             <div className={styles.inputItem}>
               <label>
                 Wall title <span>*</span>
               </label>
-              <input type="text" placeholder="your title here" />
+              <input type="text" placeholder="your title here" required />
             </div>
             <div className={styles.inputItem}>
               <label>Description</label>
               <textarea placeholder="Description"></textarea>
             </div>
             <div className={styles.btn_wrapper}>
-              <button>Back</button>
-              <button>Save wall</button>
+              <button className={styles.backWall_btn}>
+                <img src="asjk" alt="btn" />
+                Back
+              </button>
+              <button className={styles.saveWall_btn}>Save wall</button>
             </div>
           </form>
         </div>
